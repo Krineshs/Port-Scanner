f0b7d605554351624e09816ca78101b86a56f9d6
Krineshs <shsh_krinesh@yahoo.co.nz>
2022-07-18 17:40:12 +1200

    Updated
    - Added function to add custom ports
    - Removed bug, used to start from 0 - now starts from 1

--- port-scanner.py
+++ port-scanner.py
@@ -1,14 +1,15 @@
 import socket
 
-startport = ""
-endport = ""
 Address = 0
 
 Address = input('Please input the Local Ip Adress you would like to scan: ')
-#startport = input('Please input the Port you would like to START scanning from')
-#endport = input('Please input the Port you would like to END scanning from')
+startport = input('Please input the Port you would like to START scanning from: ')
+endport = input('Please input the Port you would like to END scanning from: ')
 
-for port in range(7777, 7779):
+x = int(startport)
+y = int(endport)+1
+
+for port in range(x, y):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((Address, port))
     if result == 0:
