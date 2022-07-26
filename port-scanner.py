247e3acedb8595b469af066a8df5a19e1d297ee5
Krineshs <shsh_krinesh@yahoo.co.nz>
2022-07-26 13:35:35 +1200

    Beta GUI for portscanner

--- port-scanner.py
+++ port-scanner.py
@@ -1,17 +1,29 @@
 import socket
 
-Address = input('Please input the Local Ip Address you would like to scan: ')
-startport = input('Please input the Port you would like to START scanning from: ')
-endport = input('Please input the Port you would like to END scanning from: ')
+import tkinter as tk
 
-x = int(startport)
-y = int(endport)+1
+win = tk.Tk()
+win.title("Port Scanner")
+win.resizable(False, False)
 
-for port in range(x, y):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((Address, port))
-    if result == 0:
-        print("Port " + str(port) + " is open")
-    else:
-        print("Port " + str(port) + " is closed")
-    sock.close()
+window_height = 500
+window_width = 900
+
+screen_width = win.winfo_screenwidth()
+screen_height = win.winfo_screenheight()
+
+xcord = int((screen_width/2)-(window_width))
+ycord = int((screen_height/2)-(window_height/2))
+
+win.geometry("{}x{}+{}+{}".format(window_width, window_height, xcord, ycord))
+#x = int(startport)
+#y = int(endport) + 1
+
+#for port in range(x, y):
+ #   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  #  result = sock.connect_ex((Address, port))
+   # if result == 0:
+   #     print("Port " + str(port) + " is open")
+    #else:
+     #   print("Port " + str(port) + " is closed")
+    #sock.close()
