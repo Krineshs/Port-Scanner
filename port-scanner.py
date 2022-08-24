2b8d08abe5ec58d1c9af8050bb6d4c45ca3b63c6
Krineshs <shsh_krinesh@yahoo.co.nz>
2022-08-25 11:58:56 +1200

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -1,21 +1,7 @@
 import socket
 
-import tkinter as tk
 
-win = tk.Tk()
-win.title("Port Scanner")
-win.resizable(False, False)
 
-window_height = 500
-window_width = 900
-
-screen_width = win.winfo_screenwidth()
-screen_height = win.winfo_screenheight()
-
-xcord = int((screen_width/2)-(window_width))
-ycord = int((screen_height/2)-(window_height/2))
-
-win.geometry("{}x{}+{}+{}".format(window_width, window_height, xcord, ycord))
 #x = int(startport)
 #y = int(endport) + 1
 
